b7d0e352c2f6c8ee191fce23c0aecad885a0e9a4
joshua-inf <jsibanda407@gmial.com>
2023-07-21 09:49:11 +0200

    ok one more thing

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const  App = () => {
             <Navigation/>
             <div>
               <Routes>
-                <Route path='pharmarcyapp/'  element={<Home/>}/>
+                <Route path='/pharmarcyapp/'  element={<Home/>}/>
               </Routes>
             </div>
             <Footer/>
